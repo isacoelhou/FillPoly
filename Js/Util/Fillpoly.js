4a5e934366276d90d26d0bf6bf07842fecbd9ca9
isacoelhou <isadora.araujo1@unioeste.br>
2024-10-18 10:03:22 -0300

    solve NAN

--- Js/Util/Fillpoly.js
+++ Js/Util/Fillpoly.js
@@ -15,61 +15,39 @@ class Teste {
     this.arestas.push({ x1, y1, x2, y2 });
   }
 
-  cria_arestas(){
-    for(let i = 0; i < this.points.length; i++){
-      if (i == this.points.length - 1) {
-        this.addAresta(this.points[i].x, this.points[i].y, this.points[0].x, this.points[0].y);
-      } else {
-        this.addAresta(this.points[i].x, this.points[i].y, this.points[i+1].x, this.points[i+1].y);
-      }
-    }
+  cria_arestas() {
+    this.points.forEach((point, i) => {
+      const nextPoint = this.points[(i + 1) % this.points.length];
+      this.addAresta(point.x, point.y, nextPoint.x, nextPoint.y);
+    });
   }
-
-  swap_arestas(i){
-    let aux1 = this.arestas[i].x1;
-    let aux2 = this.arestas[i].y1;
-
-    this.arestas[i].x1 = this.arestas[i].x2;
-    this.arestas[i].y1 = this.arestas[i].y2;
-
-    this.arestas[i].x2 = aux1;
-    this.arestas[i].y2 = aux2;
-  }
-
-  fillpoly() {
-    const pointsy = this.points.map(point => point.y);
-    let ymin = Math.min(...pointsy);
-    let ymax = Math.max(...pointsy);
-
-    const N_scanlines = ymax - ymin;
-    const Scan_lines = new Array(N_scanlines);
-    const coeficiente = new Array(this.arestas.length); 
-    const inters = Array.from({ length: ymax - ymin }, () => []);      
-
-    for(let i = 0; i < this.arestas.length; i++) {
-      if(this.arestas[i].y1 == this.arestas[i].y2) {
-        continue;
-      } 
-      
-      if(this.arestas[i].y1 > this.arestas[i].y2) {
-        this.swap_arestas(i);
-      }
-      
-      coeficiente[i] = (this.arestas[i].x2 - this.arestas[i].x1) / (this.arestas[i].y2 - this.arestas[i].y1);
-
-      let x = parseFloat(this.arestas[i].x1);
-      let y = parseFloat(this.arestas[i].y1);
-      let index = Math.floor(this.arestas[i].y1 - ymin); 
   
-      while (y < this.arestas[i].y2) {
-        inters[index].push(x);
-        x += coeficiente[i]; 
-        y += 1;
-        index += 1;
-      }
-    }
+  swap_arestas(i) {
+    [this.arestas[i].x1, this.arestas[i].x2] = [this.arestas[i].x2, this.arestas[i].x1];
+    [this.arestas[i].y1, this.arestas[i].y2] = [this.arestas[i].y2, this.arestas[i].y1];
+  }
+  
+  fillpoly() {
+    const ymin = Math.min(...this.points.map(p => p.y));
+    const ymax = Math.max(...this.points.map(p => p.y));
+    const inters = Array.from({ length: ymax - ymin }, () => []);
+  
+    this.arestas.forEach((aresta, i) => {
 
-    console.log(inters); 
+      if (aresta.y1 === aresta.y2) return; 
+      if (aresta.y1 > aresta.y2) this.swap_arestas(i);  
+      
+      const { x1, y1, x2, y2 } = this.arestas[i];
+      const coeficiente = (x2 - x1) / (y2 - y1);
+      
+      let x = x1;
+      let index = Math.floor(y1 - ymin);
+  
+      for (let y = y1; y < y2; y++) {
+        inters[index++].push(x);
+        x += coeficiente;
+      }
+    });
   }
 }
       
